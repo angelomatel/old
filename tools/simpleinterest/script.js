567e3d429c4154a963f3f56aa7f4a37e4af0a2c5
absolutezero01 <mateljohnangelo@gmail.com>
2018-11-09 21:08:24 +0800

    finally im finished on that one thing
    i slacked off

--- tools/simpleinterest/script.js
+++ tools/simpleinterest/script.js
@@ -1,21 +1,23 @@
+var selectedInput;
+
 function solveForChange() {
     var textInputBox = ["inputInterest","inputPrincipal","inputTime","inputRate"];
     var value = document.getElementById('solveSelection').value;
     switch(value) {
         case 'i':
-            var selectedInput = 'inputInterest';
+            selectedInput = 'inputInterest';
             document.getElementById('formulaBlock').innerHTML = 'I = P × t × r';
         break;
         case 'p':
-            var selectedInput = 'inputPrincipal';
+            selectedInput = 'inputPrincipal';
             document.getElementById('formulaBlock').innerHTML = 'P = I / r × t';
         break;
         case 't':
-            var selectedInput = 'inputTime';
+            selectedInput = 'inputTime';
             document.getElementById('formulaBlock').innerHTML = 't = I / P × r';
         break;
         case 'r':
-            var selectedInput = 'inputRate';
+            selectedInput = 'inputRate';
             document.getElementById('formulaBlock').innerHTML = 'r = I / P × t';
         break;
         default:
@@ -31,3 +33,45 @@ function solveForChange() {
         document.getElementById(textInputBox[i]).value = '';
     }
 }
+
+$("#inputInterest,#inputPrincipal,#inputTime,#inputRate").on('change keydown paste input', function(){
+    var textInterest = document.getElementById('inputInterest').value
+    var textPrincipal = document.getElementById('inputPrincipal').value
+    var textTime = document.getElementById('inputTime').value
+    var textRate = document.getElementById('inputRate').value
+    
+    console.log(selectedInput)
+    switch(selectedInput) {
+        case 'inputInterest':
+        if(!textPrincipal && !textTime && !textRate) {}
+        else{
+            var ans = textPrincipal * textTime * textRate;
+            document.getElementById('solution-answer').innerHTML = ans.toLocaleString('en-PH', {currency: 'PHP', style: 'currency'});
+        }
+        break;
+        case 'inputPrincipal':
+        if(!textInterest && !textTime && !textRate) {}
+        else{
+            console.log('meow')
+            var ans = textInterest / (textTime * textRate);
+            document.getElementById('solution-answer').innerHTML = ans;
+        }
+        break;
+        case 'inputTime':
+        if(!textPrincipal && !textInterest && !textRate) {}
+        else{
+            var ans = textInterest / (textPrincipal * textRate);
+            document.getElementById('solution-answer').innerHTML = ans;
+        }
+        break;
+        case 'inputRate':
+        if(!textPrincipal && !textTime && !textInterest) {}
+        else{
+            var ans = textInterest / (textPrincipal * textTime);
+            document.getElementById('solution-answer').innerHTML = ans;
+        }
+        break;
+        default:
+
+    }
+});
